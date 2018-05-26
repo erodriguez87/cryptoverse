6c940132d2a0556d32ecb83f3ca2c2d4c3a8bf7d
Rodriguez <Eddie.Rodriguez@cpk.jmhuber.com>
2018-05-26 11:18:50 -0400

    initial commit of the SQL schema, three test records and the connection file

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO cryptoverse_db.cryptoverse_learn (id, cryptoId, name, github, website, shortDesc, features, markets, disadvantages, started) 
+
+VALUES ('1', 'btc', 'bitcoin', 'https://github.com/bitcoin/bitcoin', 'https://bitcoin.org/en/', 'the grand daddy', 'fast currency', 'currency', 'expensive transactions, slow', ' 2009');
+
+INSERT INTO cryptoverse_db.cryptoverse_learn (id, cryptoId, name, github, website, shortDesc, features, markets, disadvantages, started) 
+
+VALUES ('2', 'eth', 'ethereum', 'https://github.com/ethereum/', 'https://www.ethereum.org/', 'app platform', 'application platform, contracts', 'contracts,currency,application', 'scaling issues', ' 2014');
+
+INSERT INTO cryptoverse_db.cryptoverse_learn (id, cryptoId, name, github, website, shortDesc, features, markets, disadvantages, started) 
+
+VALUES ('3', 'xrp', 'ripple', 'https://github.com/ripple', 'https://ripple.com/', 'a blockchain established to facilitate transfer of money across country borders', 'baking, transfer', 'contracts,currency,application', 'centralized', ' 2014');
