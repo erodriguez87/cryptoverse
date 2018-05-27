2a694972121ed08a639737f167da9cd65f46a4fa
Rodriguez <Eddie.Rodriguez@cpk.jmhuber.com>
2018-05-26 23:11:11 -0400

    Replacing the old Schema.sql file with sequelize define

--- db/schema.sql
+++ db/schema.sql
@@ -5,25 +5,25 @@ CREATE DATABASE cryptoverse_db;
 USE cryptoverse_db;
 
 -- table set up for facts about each of the cryptocurrencies that users can learn about. These are research items that are manually saved to the database by administrators
-CREATE TABLE cryptoverse_learn (
-  id INTEGER NOT NULL AUTO_INCREMENT,
-  cryptoId VARCHAR(10) NOT NULL,
-  name VARCHAR(50) NOT NULL,
-  github VARCHAR(50) NOT NULL,
-  website VARCHAR(50) NOT NULL,
-  shortDesc TEXT(100) NOT NULL,
-  features VARCHAR(200),
-  markets VARCHAR(100) NOT NULL,
-  disadvantages VARCHAR(200),
-  started VARCHAR(20),
-  PRIMARY KEY (id)
-);--
+-- CREATE TABLE cryptoverse_learn (
+--   id INTEGER NOT NULL AUTO_INCREMENT,
+--   cryptoId VARCHAR(10) NOT NULL,
+--   name VARCHAR(50) NOT NULL,
+--   github VARCHAR(50) NOT NULL,
+--   website VARCHAR(50) NOT NULL,
+--   shortDesc TEXT(100) NOT NULL,
+--   features VARCHAR(200),
+--   markets VARCHAR(100) NOT NULL,
+--   disadvantages VARCHAR(200),
+--   started VARCHAR(20),
+--   PRIMARY KEY (id)
+-- );--
 
 -- table set up to save user preferred coins, amounts they hold etc...
-CREATE TABLE cryptoverse_users (
-  id INTEGER NOT NULL AUTO_INCREMENT,
-  name VARCHAR(100) NOT NULL,
-  email VARCHAR(100) NOT NULL,
-  password VARCHAR(100) NOT NULL,
-  PRIMARY KEY (id)
-);
+-- CREATE TABLE cryptoverse_users (
+--   id INTEGER NOT NULL AUTO_INCREMENT,
+--   name VARCHAR(100) NOT NULL,
+--   email VARCHAR(100) NOT NULL,
+--   password VARCHAR(100) NOT NULL,
+--   PRIMARY KEY (id)
+-- );
