bce44b09814fbc5a7448a5cd3f78e59c9f1d0ea1
SusanLDavenport <susanldavenport@gmail.com>
2018-05-29 20:00:03 -0400

    more css, draft images

--- routes/news-api-routes.js
+++ routes/news-api-routes.js
@@ -2,15 +2,14 @@ const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('4f9397d3c24942eb92519616eedfbc23');
 
 newsapi.v2.everything({
-  q: 'cryptocurrency',
+  q: 'crypto currency',
   sources: 'bbc-news,the-verge',
-  domains: 'bbc.co.uk, techcrunch.com',
+  domains: 'techcrunch.com, coinbaseapi.com',
   language: 'en',
   sortBy: 'publishedAt',
   page: 2
 }).then(response => {
   console.log(response);
-  $("#news-api-body").text(JSON.stringify(response));
   /*
     {
       status: "ok",
