0a456243163bb236d3d77367afc12226ca8b28da
Allen Welch <allenjwelch@gmail.com>
2018-05-31 10:03:48 -0400

    added logic to add coin info to DOM

--- public/assets/js/learnLogic.js
+++ public/assets/js/learnLogic.js
@@ -6,48 +6,36 @@ $(document).ready(function(){
     getData(coin); 
     
 
-
-
-    // switch(coin) {  
-    //   case "ada":
-    //     console.log('Cardano'); 
-    //     break;
-    //   case "bat":
-    //     console.log('Basic Attention Token'); 
-    //     break;
-    //   case "btc":
-    //     console.log('Bitcoin'); 
-    //     break;
-    //   case "doge":
-    //     console.log('Dogecoin'); 
-    //     break;
-    //   case "eth":
-    //     console.log('Ethereum');  
-    //     break;
-    //   case "ltc":
-    //     console.log('Litecoin');  
-    //     break;
-    //   case "trx":
-    //     console.log('TronCoin'); 
-    //     break;
-    //   case "ven":
-    //     console.log('VeChain');  
-    //     break;
-    //   case "xlm":
-    //     console.log('Stellar');  
-    //     break;
-    //   case "xrp":
-    //     console.log('Ripple');  
-    //     break;
-    //   default:
-    //       code 
-    // }
+    // "id": 1,
+    // "cryptoId": "btc",
+    // "name": "bitcoin",
+    // "github": "https://github.com/bitcoin/bitcoin",
+    // "website": "https://bitcoin.org/en/",
+    // "shortDesc": "the grand daddy",
+    // "features": "fast currency",
+    // "markets": "currency",
+    // "disadvantages": "expensive transactions, slow",
+    // "started": "2009",
+    // "createdAt": "2018-05-31T13:27:59.000Z",
+    // "updatedAt": "2018-05-31T13:27:59.000Z"
 
   });
 
   function getData(coin) {
-    $.get("/api/learn/" + coin, function(data) {
-      console.log(data.name); 
+    $.get("/api/learn/" + coin, function(coin) {
+      console.log(coin.name); 
+      let infoDiv  = $(".coinInfo"); 
+    $("#name").html(`${coin.name} (${coin.cryptoId})`); 
+    $("#features").html(coin.features); 
+    $("#markets").html(coin.markets); 
+    $("#start").html(coin.started); 
+    $("#description").html(coin.shortDesc); 
+    $("#disadvantage").html(coin.disadvantages); 
+    $("a#web").attr('href', coin.website); 
+    $("a#git").attr('href', coin.github); 
+
+
+
 
     }); 
     // $.ajax({
@@ -61,97 +49,97 @@ $(document).ready(function(){
 });
 
 // Pie Chart Logic
-  (function(d3) {
-    'use strict';
+  // (function(d3) {
+  //   'use strict';
 
-    let width = 360;
-    let height = 360;
-    let radius = Math.min(width, height) / 2;
-    let donutWidth = 75;
-    let legendRectSize = 18;
-    let legendSpacing = 4;
+  //   let width = 360;
+  //   let height = 360;
+  //   let radius = Math.min(width, height) / 2;
+  //   let donutWidth = 75;
+  //   let legendRectSize = 18;
+  //   let legendSpacing = 4;
 
-    let color = d3.scaleOrdinal().range(['#494E6B','#192231']);
+  //   let color = d3.scaleOrdinal().range(['#494E6B','#192231']);
 
-    let svg = d3.select('#chart')
-      .append('svg')
-      .attr('width', width)
-      .attr('height', height)
-      .append('g')
-      .attr('transform', 'translate(' + (width / 2) +
-        ',' + (height / 2) + ')');
+  //   let svg = d3.select('#chart')
+  //     .append('svg')
+  //     .attr('width', width)
+  //     .attr('height', height)
+  //     .append('g')
+  //     .attr('transform', 'translate(' + (width / 2) +
+  //       ',' + (height / 2) + ')');
 
-    let arc = d3.arc()
-      .innerRadius(radius - donutWidth)
-      .outerRadius(radius);
+  //   let arc = d3.arc()
+  //     .innerRadius(radius - donutWidth)
+  //     .outerRadius(radius);
 
-    let pie = d3.pie()
-      .value(function(d) { return d.count; })
-      .sort(null);
+  //   let pie = d3.pie()
+  //     .value(function(d) { return d.count; })
+  //     .sort(null);
 
-    let tooltip = d3.select('#chart')                               
-      .append('div')                                                
-      .attr('class', 'tooltip');                                    
+  //   let tooltip = d3.select('#chart')                               
+  //     .append('div')                                                
+  //     .attr('class', 'tooltip');                                    
 
-    tooltip.append('div')                                           
-      .attr('class', 'label');                                      
+  //   tooltip.append('div')                                           
+  //     .attr('class', 'label');                                      
 
-    tooltip.append('div')                                           
-      .attr('class', 'count');                                      
+  //   tooltip.append('div')                                           
+  //     .attr('class', 'count');                                      
 
-    tooltip.append('div')                                           
-      .attr('class', 'percent');                                    
+  //   tooltip.append('div')                                           
+  //     .attr('class', 'percent');                                    
 
-    let dataset = [
-      { label: 'BTC', count:34},
-      { label: 'All Others', count: 76}
-      ];
+  //   let dataset = [
+  //     { label: 'BTC', count:34},
+  //     { label: 'All Others', count: 76}
+  //     ];
 
-      let path = svg.selectAll('path')
-        .data(pie(dataset))
-        .enter()
-        .append('path')
-        .attr('d', arc)
-        .attr('fill', function(d, i) {
-          return color(d.data.label);
-        });
+  //     let path = svg.selectAll('path')
+  //       .data(pie(dataset))
+  //       .enter()
+  //       .append('path')
+  //       .attr('d', arc)
+  //       .attr('fill', function(d, i) {
+  //         return color(d.data.label);
+  //       });
 
-      path.on('mouseover', function(d) {                            
-        let total = d3.sum(dataset.map(function(d) {                
-          return d.count;                                           
-        }));                                                        
-        tooltip.select('.label').html(d.data.label);                
-        tooltip.select('.count').html(d.data.count + '%');                     
-        tooltip.style('display', 'block');                          
-      });                                                           
+  //     path.on('mouseover', function(d) {                            
+  //       let total = d3.sum(dataset.map(function(d) {                
+  //         return d.count;                                           
+  //       }));                                                        
+  //       tooltip.select('.label').html(d.data.label);                
+  //       tooltip.select('.count').html(d.data.count + '%');                     
+  //       tooltip.style('display', 'block');                          
+  //     });                                                           
 
-      path.on('mouseout', function() {                              
-        tooltip.style('display', 'none');                           
-      });                                                           
+  //     path.on('mouseout', function() {                              
+  //       tooltip.style('display', 'none');                           
+  //     });                                                           
                                             
-      let legend = svg.selectAll('.legend')
-        .data(color.domain())
-        .enter()
-        .append('g')
-        .attr('class', 'legend')
-        .attr('transform', function(d, i) {
-          let height = legendRectSize + legendSpacing;
-          let offset =  height * color.domain().length / 2;
-          let horz = -2 * legendRectSize;
-          let vert = i * height - offset;
-          return 'translate(' + horz + ',' + vert + ')';
-        });
+  //     let legend = svg.selectAll('.legend')
+  //       .data(color.domain())
+  //       .enter()
+  //       .append('g')
+  //       .attr('class', 'legend')
+  //       .attr('transform', function(d, i) {
+  //         let height = legendRectSize + legendSpacing;
+  //         let offset =  height * color.domain().length / 2;
+  //         let horz = -2 * legendRectSize;
+  //         let vert = i * height - offset;
+  //         return 'translate(' + horz + ',' + vert + ')';
+  //       });
 
-      legend.append('rect')
-        .attr('width', legendRectSize)
-        .attr('height', legendRectSize)
-        .style('fill', color)
-        .style('stroke', color);
+  //     legend.append('rect')
+  //       .attr('width', legendRectSize)
+  //       .attr('height', legendRectSize)
+  //       .style('fill', color)
+  //       .style('stroke', color);
 
-      legend.append('text')
-        .attr('x', legendRectSize + legendSpacing)
-        .attr('y', legendRectSize - legendSpacing)
-        .text(function(d) { return d; });
+  //     legend.append('text')
+  //       .attr('x', legendRectSize + legendSpacing)
+  //       .attr('y', legendRectSize - legendSpacing)
+  //       .text(function(d) { return d; });
 
 
-  })(window.d3);
+  // })(window.d3);
