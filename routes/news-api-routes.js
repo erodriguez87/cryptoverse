6be0d21570a847d2cc4a515400fef61f79091c33
SusanLDavenport <susanldavenport@gmail.com>
2018-06-02 14:27:44 -0400

    news divs working

--- routes/news-api-routes.js
+++ routes/news-api-routes.js
@@ -1,26 +1,19 @@
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('4f9397d3c24942eb92519616eedfbc23');
 
-module.exports = function(app) {
-  var newsRes; 
-  console.log('inside news API');
+  module.exports = function(app) {
+    var newsRes; 
+    console.log('inside news API');
 
-  app.get('/api/news', function(req, res){
-    newsapi.v2.everything({
-      sources: 'crypto-coins-news',
-      language: 'en',
-      sortBy: 'publishedAt',
-      page: 2
-    }).then(response => {
-      newsRes = response;
-      res.json(newsRes);
-      // console.log(newsRes);
-      /*
-        {
-          status: "ok",
-          articles: [...]
-        }
-      */
+    app.get('/api/news', function(req, res){
+      newsapi.v2.everything({
+        sources: 'crypto-coins-news',
+        language: 'en',
+        sortBy: 'publishedAt',
+        page: 2
+      }).then(newsRes => {
+        res.json(newsRes);
+        console.log(newsRes);
+      });
     });
-  });
-}
+  }
