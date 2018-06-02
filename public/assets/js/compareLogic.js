5acdae7098ca31da40e8ff82df2af8c3382d84c5
Eddie Rodriguez <erodriguez87@gmail.com>
2018-06-01 23:16:24 -0400

    updating compare logic

--- public/assets/js/compareLogic.js
+++ public/assets/js/compareLogic.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
   $('select').formSelect();
+  $(".compareBtn").on("click", function(event) {
+    let coin = $(this).attr("id"); 
+    // getData(coin); 
+    // getCrypto(coin);
+
+  });
+
+  function getData(coin) {
+    $.get("/api/compare/" + coin, function(coin) {
+      // console.log(coin.name); 
+      let infoDiv  = $(".coinInfo"); 
+    $("#name1").html(`${coin.name} (${coin.cryptoId})`); 
+    }); 
+    // $.ajax({
+    //   method: "GET",
+    //   url: "/api/learn/" + coin
+    // }).then(function(data) {
+    // });
+    
+  };
+
 });
