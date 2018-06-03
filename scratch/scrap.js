8ed801e805e8028861bdf1e6712e94d3d4ec5056
Eddie Rodriguez <erodriguez87@gmail.com>
2018-06-03 14:48:31 -0400

    updating the seed data so all coins work on learn page

--- scratch/scrap.js
+++ scratch/scrap.js
@@ -5,7 +5,7 @@ listSymbols = {
 }
 request("https://api.coinmarketcap.com/v2/listings/", function(error, response, body) {
   if (!error && response.statusCode === 200) {
-    for (i=500; i < 600; i++){
+    for (i=900; i < 1000; i++){
     listSymbols.symbol.push(JSON.parse(body).data[i].symbol)
     listSymbols.id.push(JSON.parse(body).data[i].id)
     };
@@ -13,40 +13,6 @@ request("https://api.coinmarketcap.com/v2/listings/", function(error, response, 
   console.log(listSymbols);
 });
 // res.json(listSymbols)
-
-
-
-  
-
-  // app.post("/api/clear", function() {
-  //   // Empty out the arrays of data
-  //   tableData = [];
-  //   waitListData = [];
-
-  //   console.log(tableData);
-  // });
-// };
-
-
-  // // The second call gives the second 100 if we need it
-  // let tickerUrl2 = "https://api.coinmarketcap.com/v2/ticker/?start=101&limit=200&sort=volume_24h";
-  // request(tickerUrl2, function(error, response, body) {
-  //   // If the request is successful
-  //   if (!error && response.statusCode === 200) {
-  //     console.log("Ticker Part 2: " + JSON.parse(body));
-  //   }
-//   // });
-  
-// // Get request for to look up one speecific coin
-//   let chosenCoin = "1" // need to pass the id of the coin we want to search for into this variable. this is the number and not 3 letter code
-//   let specificCoinUrl = `https://api.coinmarketcap.com/v2/ticker/${chosenCoin}/`;
-
-//   request(specificCoinUrl, function(error, response, body) {
-//     // If the request is successful
-//     if (!error && response.statusCode === 200) {
-//       console.log("specific cryptos: " + JSON.parse(body));
-//     }
-//   });
 
 // //global api to get data for the whole market. This includes the total value
   
