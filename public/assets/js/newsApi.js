846f74d8ed83b0ed9c7c4998a78a34d4a9f268f0
SusanLDavenport <susanldavenport@gmail.com>
2018-06-04 20:57:14 -0400

    fixed news slider

--- public/assets/js/newsApi.js
+++ public/assets/js/newsApi.js
@@ -6,20 +6,7 @@ $.ajax({url: currentURL + "/api/news", method: "GET"})
   .then(function(response){
     console.log('inside news api');
     for (var i = 0; i < response.articles.length -15; i++) {
-      // console.log('inside news for loop');
-      // var newCard = $('<li>');
-      // var newBackground = $(`<div id="articles.urlToImage"><img src="${response.articles[i].urlToImage}>`);
-      // console.log('this is the url fo rthe image ' + response.articles[i].urlToImage);
-      // var newDiv = $('<div class="caption left-align">');
-      // var newDate = $(`<div id="articles.publishedAt"><h4 class="black-text">${response.articles[i].publishedAt}</h4><br></div>`)
-      // var newTitle = $(`<div id="articles.title"><h2>${response.articles[i].title}</h2><br></div>`);
-      // var newURL = $(`<div id="articles.url"><h4><a href="${response.articles[i].url}">READ MORE</a></h4></div>`);
-      // newDiv.append(newDate); 
-      // newDiv.append(newTitle);
-      // newDiv.append(newURL);
-      // newCard.append(newBackground);
-      // newCard.append(newDiv);
-      // $(".slides").append(newCard);
+
       let imgSrc = 'img'+[i]
       console.log(imgSrc);
       $(`#${imgSrc}`).attr('src',response.articles[i].urlToImage);
@@ -27,7 +14,11 @@ $.ajax({url: currentURL + "/api/news", method: "GET"})
       let titleText = 'title' + [i]
       $(`#${titleText}`).append(response.articles[i].title);
 
-      // console.log("NEW CARD", newCard);
+      let dateText = 'date' + [i]
+      $(`#${dateText}`).append(response.articles[i].publishedAt);
+
+      let linkText = 'url' + [i]
+      $(`#${linkText}`).append('<a href="'+(response.articles[i].url)+'">Read More</a>');
   };
 })
 }
