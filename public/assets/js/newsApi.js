466518d54ffc0ae260200d67f1df0c72c85c5c8f
Eddie Rodriguez <erodriguez87@gmail.com>
2018-06-04 20:04:53 -0400

    updating the slider

--- public/assets/js/newsApi.js
+++ public/assets/js/newsApi.js
@@ -4,21 +4,30 @@ var currentURL = window.location.origin;
 
 $.ajax({url: currentURL + "/api/news", method: "GET"})
   .then(function(response){
-    // console.log(response);
+    console.log('inside news api');
     for (var i = 0; i < response.articles.length -15; i++) {
-      var newCard = $('<li>');
-      var newBackground = $(`<span id="articles.urlToImage"><img src="${response.articles[i].urlToImage}>`);
-      var newDiv = $('<div class="caption left-align">');
-      var newDate = $(`<span id="articles.publishedAt"><h4 class="black-text">${response.articles[i].publishedAt}</h4><br></span>`)
-      var newTitle = $(`<span id="articles.title"><h2>${response.articles[i].title}</h2><br></span>`);
-      var newURL = $(`<span id="articles.url"><h4><a href="${response.articles[i].url}">READ MORE</a></h4></span>`);
-      newDiv.append(newDate); 
-      newDiv.append(newTitle);
-      newDiv.append(newURL);
-      newCard.append(newBackground);
-      newCard.append(newDiv);
-      $(".slides").append(newCard);
-      console.log("NEW CARD", newCard);
+      // console.log('inside news for loop');
+      // var newCard = $('<li>');
+      // var newBackground = $(`<div id="articles.urlToImage"><img src="${response.articles[i].urlToImage}>`);
+      // console.log('this is the url fo rthe image ' + response.articles[i].urlToImage);
+      // var newDiv = $('<div class="caption left-align">');
+      // var newDate = $(`<div id="articles.publishedAt"><h4 class="black-text">${response.articles[i].publishedAt}</h4><br></div>`)
+      // var newTitle = $(`<div id="articles.title"><h2>${response.articles[i].title}</h2><br></div>`);
+      // var newURL = $(`<div id="articles.url"><h4><a href="${response.articles[i].url}">READ MORE</a></h4></div>`);
+      // newDiv.append(newDate); 
+      // newDiv.append(newTitle);
+      // newDiv.append(newURL);
+      // newCard.append(newBackground);
+      // newCard.append(newDiv);
+      // $(".slides").append(newCard);
+      let imgSrc = 'img'+[i]
+      console.log(imgSrc);
+      $(`#${imgSrc}`).attr('src',response.articles[i].urlToImage);
+
+      let titleText = 'title' + [i]
+      $(`#${titleText}`).append(response.articles[i].title);
+
+      // console.log("NEW CARD", newCard);
   };
 })
 }
